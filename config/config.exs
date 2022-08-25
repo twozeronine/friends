b99fcb49f6605c69a4115157d76cb84bbd5a6e9b
twozeronine <amdusias209@gmail.com>
2022-08-25 19:15:42 +0900

    feat : people 테이블 생성

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,8 @@ import Config
 
 config :friends, Friends.Repo,
   database: "friends_repo",
-  # username: "user",
-  # password: "pass",
+  username: "postgres",
+  password: "postgres",
   hostname: "localhost"
+
+config :friends, ecto_repos: [Friends.Repo]
