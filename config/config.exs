25b94acf546d9c65a942d186bedc6abb5f60125f
twozeronine <amdusias209@gmail.com>
2022-08-28 14:50:48 +0900

    feat : 포트와 풀사이즈 설정

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,9 @@ config :friends, Friends.Repo,
   database: "friends_repo",
   username: "postgres",
   password: "postgres",
-  hostname: "localhost"
+  hostname: "localhost",
+  port: 5432,
+  # 풀 사이즈를 설정하면 DB에 접근 할 수 있는 여러가지 프로세스를 생성할수있다.
+  pool_size: 20
 
 config :friends, ecto_repos: [Friends.Repo]
